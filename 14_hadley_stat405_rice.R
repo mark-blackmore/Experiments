3ec8c06bc1ec4ab65e75ac3ca0fc2b3a00d46fc6
Mark Blackmore <mblackmo@yahoo.com>
2017-10-07 16:54:20 -0700

    Progress on Lecture 14.

--- 14_hadley_stat405_rice.R
+++ 14_hadley_stat405_rice.R
@@ -58,7 +58,7 @@ str(strsplit(headers[1], "\n")[1])
 str(strsplit(headers[1], "\n")[[1]])
 str(strsplit(headers, "\n"))
 
-#' ## Exercise
+#' ### Exercise
 #' Write a small function that given a single header
 #' field splits it into name and contents. Do you
 #' want to use str_split(), or str_locate() &
@@ -85,11 +85,62 @@ f1(test1)
 f2(test1)
 f2(test1)
 
-#' ## Next Steps
+#' ### Next Steps  
 #' We split the content into header and
 #' body. And split up the header into fields.
 #' Both of these tasks used fixed strings.
 #' What if the pattern we need to match is
-#' more complicated? 
+#' more complicated?   
+#'  
+#' ### Matching Challenges  
+#' * How could we match a phone number?
+#' * How could we match a date?
+#' * How could we match a time?
+#' * How could we match an amount of
+#' money?
+#' * How could we match an email address?
+#' 
+#' ## Regular Expressions  
+#' Each of these types of data have a fairly
+#' regular pattern that we can easily pick out by eye
+#' Today we are going to learn about regular
+#' expressions, which are an extremely
+#' concise language for describing patterns.
+#' 
+#' #### First challenge  
+#' * Matching phone numbers
+#' * How are phone numbers normally written?
+#' * How can we describe this format?
+#' * How can we extract the phone numbers?
+#' 
+#' ##### Phone numbers
+#' * 10 digits, normally grouped 3-3-4
+#' * Separated by space, - or ()
+#' * How can we express that with a computer
+#' program? We'll use regular expressions
+#'
+#  [0-9]: matches any number between 0 and 9
+#  [- ()]: matches -, space, ( or )
+#' 
+phone <- "[ (][0-9][0-9][0-9][- )][0-9][0-9][0-9][- ][0-9][0-9][0-9][0-9]"
+phone2 <- "[0-9]{3}[- .][0-9]{3}[- .][0-9]{4}"
+phone3 <- "[(][0-9]{3}[)][- .][0-9]{3}[- ()][0-9]{4}"
+# test <- body[10]
+# cat(test)
+# 
+# str_detect(test, phone2)
+# str_locate(test, phone2)
+# str_locate_all(test, phone2)
+# 
+# str_extract(test, phone2)
+# str_extract_all(test, phone2)
 
+#' What do these regular expression match?
+mystery1 <- "[0-9]{5}(-[0-9]{4})?"
+mystery2 <- "[0-9]{3}-[0-9]{2}-[0-9]{4}"
+mystery3 <- "[A-Z0-9._%+-]+@[A-Z0-9.-]+\\.[A-Z]{2,4}"
+mystery4 <- "https?://[a-z]+([a-z0-9-]*[a-z0-9]+)?(\\.([a-z]+([a-z0-9-]*[a-z0-9]+)?)+)*"
+#' Think about them first, then input to http://strfriend.com/
+#' or http://xenon.stanford.edu/~xusch/regexp/analyzer.html
+#' (select java for language - it's closest to R for regexps)
 
